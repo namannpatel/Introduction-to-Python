a831fb61e550686b57296afef8ba11cde9ccbdc9
Naman Patel <62150420+thenamanpatel@users.noreply.github.com>
2023-03-19 22:48:04 +0530

    Update P02_Series.py

--- Pandas/P02_Series.py
+++ Pandas/P02_Series.py
@@ -3,10 +3,9 @@
 #A Series is a one-dimensional array containing a sequence of values of any data type (int, float, list, string, etc)
 
 #to import pandas library:
-
 import pandas as pd
 
-#Creation of Series
+#1. Creation of Series
 
 #(A) Creation of Series from Scalar Values:
 series1 = pd.Series([10,20,30])
@@ -29,3 +28,33 @@ print(series3)
 #3     4 
 #dtype: int32
 
+#we can use letters or strings as indices
+series4 = pd.Series(array1, index = ["Jan", "Feb", "Mar", "Apr"])
+print(series4)
+
+#Jan    1
+#Feb    2
+#Mar    3
+#Apr    4
+#dtype: int32
+
+#(C) Creation of Series from Dictionary
+dict1 = {'India': 'NewDelhi', 'UK': 'London', 'Japan': 'Tokyo'}
+print(dict1)      #Display the dictionary
+#{'India': 'NewDelhi', 'UK': 'London', 'Japan': 'Tokyo'}
+series8 = pd.Series(dict1)
+print(series8)    #Display the series
+
+#India    NewDelhi
+#UK         London
+#Japan       Tokyo
+#dtype: object
+
+#2.Accessing Elements of a Series
+
+#(A) Indexing
+#usage of positional index for accessing a value
+seriesNum = pd.Series([10,20,30])
+seriesNum[2]
+
+#30
