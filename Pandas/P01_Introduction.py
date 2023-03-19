da10581dc527945fd30512ba6a3b8159632cc735
Naman Patel <62150420+thenamanpatel@users.noreply.github.com>
2023-03-19 22:34:27 +0530

    Create P01_Introduction.py
    Intro and Basics of Pandas

---
+++ Pandas/P01_Introduction.py
@@ -0,0 +1,17 @@
+# Author: NAMAN PATEL
+
+#PANDAS (PANel DAta) is a high-level data manipulation tool used for analysing data. 
+#It is very easy to import and export data using Pandas library which has a very rich set of functions. 
+#It is built on packages like NumPy and Matplotlib and gives us a single, convenient place to do most of our data analysis and visualisation work.
+
+#Pandas has three important data structures, namely – Series, DataFrame and Panel to make the process of analysing data organised, effective and efficient.
+
+#Installing Pandas : pip install pandas
+
+#Data Structure in Pandas
+#A data structure is a collection of data values and operations that can be applied to that data. 
+#It enables efficient storage, retrieval and modification to the data.
+
+#Two commonly used data structures in Pandas are:
+#Series
+#DataFrame
