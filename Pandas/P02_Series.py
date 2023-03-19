942df42200d7278001757515b6a93eafe41052d4
Naman Patel <62150420+thenamanpatel@users.noreply.github.com>
2023-03-19 23:50:16 +0530

    Update P02_Series.py

--- Pandas/P02_Series.py
+++ Pandas/P02_Series.py
@@ -78,3 +78,117 @@ seriesCapCntry[['UK','USA']]
 #USA    WashingtonDC
 #dtype: object
 
+#assigning new index values
+seriesCapCntry.index=[10,20,30,40]
+print(seriesCapCntry)
+
+#10        NewDelhi
+#20    WashingtonDC
+#30          London
+#40           Paris
+#dtype: object
+
+#(B) Slicing
+#to extract a part of a series
+#syntax: <Object>[start : end]
+seriesCapCntry[1:3] #excludes the value at index position 3
+
+#USA    WashingtonDC
+#UK           London
+#dtype: object
+
+seriesCapCntry['USA' : 'France']  #includes the value at index position France
+
+#USA        WashingtonDC
+#UK               London
+#France            Paris
+#dtype: object
+
+seriesCapCntry[ : : -1] #to get the series in reverse order
+#France          Paris
+#UK             London
+#USA      WashingtonDC
+#India        NewDelhi
+#dtype: object
+
+#use slicing to modify the values of series elements
+import numpy as np
+seriesAlph = pd.Series(np.arange(10,16,1), index = ['a', 'b', 'c', 'd', 'e', 'f'])
+print(seriesAlph)
+#a    10
+#b    11
+#c    12
+#d    13
+#e    14
+#f    15
+#dtype: int32
+seriesAlph[1:3] = 50    #excludes the value at the end index position
+print(seriesAlph)
+#a    10
+#b    50
+#c    50
+#d    13
+#e    14
+#f    15
+#dtype: int32
+
+seriesAlph['c':'e'] = 500   #changes the value at the end index label
+ >>> seriesAlph             #when slicing is done using labels
+#a     10
+#b     50
+#c    500
+#d    500
+#e    500
+#f     15
+#dtype: int32
+
+#3 Attributes of Series
+
+#some attributes of Pandas series usingseriesCapCntry as an example:
+ >>> seriesCapCntry
+#India          NewDelhi
+#USA        WashingtonDC
+#UK               London
+#France            Paris
+#dtype: object
+
+#a. name attribute
+#assigns a name to the Series
+seriesCapCntry.name = ‘Capitals’
+print(seriesCapCntry)
+#India          NewDelhi
+#USA        WashingtonDC
+#UK               London
+#France            Paris
+#Name: Capitals, dtype: object
+
+#b. index.name attribute
+#assigns a name to the index of the series
+seriesCapCntry.index.name = ‘Countries’
+print(seriesCapCntry)
+#Countries
+#India          NewDelhi
+#USA        WashingtonDC
+#UK               London
+#France            Paris
+#Name: Capitals, dtype: object
+
+#c. values attribute
+#prints a list of the values in the series
+print(seriesCapCntry.values)
+#[‘NewDelhi’ ‘WashingtonDC’ ‘London’ ‘Paris’]
+
+#d. size attribute
+#prints the number of values in the Series object
+print(seriesCapCntry.size)
+#4
+
+#e. empty
+#prints True if the series is empty, and False otherwise
+seriesCapCntry.empty
+#False
+
+#Create an empty series
+seriesEmpt=pd.Series()
+seriesEmpt.empty
+#True
